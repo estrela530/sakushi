ce074df4fa4c9f4efa734aca3eb80488cbfeb71c
blue666225 <fwantazi1145@gmail.com>
2020-04-08 01:51:29 +0900

    影の物の投影を追加（まだ完全ではない）

---
+++ sin_sakushi/Assets/Scripts/Shadow/Shadow.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shadow : MonoBehaviour
+{
+    public GameObject Shadow1;
+    public GameObject Shadow2;
+    public GameObject gai;//外周部
+    public GameObject tyu;//中心部
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //影1の出現
+        if (gai.transform.rotation.y == 0)
+        {
+            Shadow1.gameObject.SetActive(true);
+        }
+        else
+        {
+            Shadow1.gameObject.SetActive(false);
+        }
+        //影2の出現
+        if (gai.transform.rotation.y == 180)
+        {
+            Shadow2.gameObject.SetActive(true);
+        }
+        else
+        {
+            Shadow2.gameObject.SetActive(false);
+        }
+
+
+    }
+}
