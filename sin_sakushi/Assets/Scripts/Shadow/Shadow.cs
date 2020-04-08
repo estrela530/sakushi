e5e7926e658bb712cbc11b45ffda400da9b04ff5
根岸 渉 <teteruru3690@gmail.com>
2020-04-09 04:46:47 +0900

    プレハブ再掲

--- sin_sakushi/Assets/Scripts/Shadow/Shadow.cs
+++ sin_sakushi/Assets/Scripts/Shadow/Shadow.cs
@@ -4,22 +4,28 @@ using UnityEngine;
 
 public class Shadow : MonoBehaviour
 {
-    public GameObject Shadow1;
-    public GameObject Shadow2;
+    [SerializeField] GameObject Shadow1;
+    [SerializeField] GameObject Shadow2;
     public GameObject gai;//外周部
     public GameObject tyu;//中心部
+    [SerializeField] ModeManager modeManager;
+
     // Start is called before the first frame update
     void Start()
-    {
-
+    { 
     }
 
     // Update is called once per frame
     void Update()
     {
-        //影1の出現
-        if (gai.transform.rotation.y == 0)
+        if (modeManager == null)
         {
+            return;
+        }
+        //影1の出現
+        if (modeManager.NowOutMode()== 2)
+        {
+            //外周部が90度の時に出現
             Shadow1.gameObject.SetActive(true);
         }
         else
@@ -27,15 +33,15 @@ public class Shadow : MonoBehaviour
             Shadow1.gameObject.SetActive(false);
         }
         //影2の出現
-        if (gai.transform.rotation.y == 180)
+        if (modeManager.NowInMode() == 3)
         {
+            //中心部が180度の時に出現
             Shadow2.gameObject.SetActive(true);
         }
         else
         {
             Shadow2.gameObject.SetActive(false);
         }
-
 
     }
 }
