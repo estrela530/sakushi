3acedf994457d5c24f753f2b0a4b82c038b22dbf
kawashita kazuma <kazuma@c08.itscom.net>
2020-04-08 10:50:17 +0900

    燃え移り（仮）追加

--- sin_sakushi/Assets/Scripts/Status/IgnitStatus.cs
+++ sin_sakushi/Assets/Scripts/Status/IgnitStatus.cs
@@ -6,21 +6,35 @@ public class IgnitStatus : MonoBehaviour
 {
     [SerializeField]
     bool ignit;
+
+    Ray ray;
+    RaycastHit hit;
+
+    
     // Start is called before the first frame update
     void Start()
     {
         ignit = false;
+        ray = new Ray(transform.position, transform.forward);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(ignit)
+        {
+            if (Physics.Raycast(ray, out hit, 160.0f)) ;
+            {
+                hit.collider.GetComponent<MeshRenderer>().material.color = Color.red;
+                Debug.DrawRay(transform.position, transform.forward * 160.0f, Color.yellow);
+            }
+        }
     }
 
     public bool GetIgnit()
     {
         return ignit;
+        //GetComponent<BoxCollider>().enabled = true;
     }
 
     public void ChangeIgnit()
