b1a19554adcc71c030a3a4b20cfeb34b90f52624
根岸 渉 <teteruru3690@gmail.com>
2020-04-09 03:43:11 +0900

    クリアの実装とプレハブの追加(台座二種と外周部、中心部、Clear)

---
+++ sin_sakushi/Assets/Scripts/Manager/ClearManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearManager : MonoBehaviour
+{
+    [SerializeField,Header("着火状態でクリアの台座のリスト")]
+    List<IgnitStatus> FirePedestals;
+
+    [SerializeField, Header("鎮火状態でクリアの台座のリスト")]
+    List<IgnitStatus> ClearPedestals;
+
+    [SerializeField,Header("クリア関係のCanvas")]
+    GameObject ClearCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ClearCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < FirePedestals.Count; i++)
+        {
+            if (!FirePedestals[i].GetIgnit())
+            {
+                ClearCanvas.SetActive(false);
+                return;
+            }
+        }
+
+        
+        for (int i = 0; i < ClearPedestals.Count; i++)
+        {
+            if (ClearPedestals[i].GetIgnit())
+            {
+                ClearCanvas.SetActive(false);
+                return;
+            }
+        }
+
+        
+
+        ClearCanvas.SetActive(true);
+    }
+}
