0cb3d4e14513b40c1e949c50d07d0355e7b66833
根岸 渉 <teteruru3690@gmail.com>
2020-04-11 23:21:25 +0900

    プロトの状態にキーを左右からADに内外変更をWに変更。今操作してるのを赤操作してないのを青く表示

---
+++ sin_sakushi/Assets/Scripts/Manager/InOutMaterial.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InOutMaterial : MonoBehaviour
+{
+    [SerializeField]
+    Transform outParent;
+
+    [SerializeField]
+    Transform inParent;
+
+    [SerializeField]
+    ModeManager modeManager;
+
+    [SerializeField]
+    Material operateMat;
+
+    [SerializeField]
+    Material notOperateMat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (modeManager.GetFalseInTrueOut())
+        {
+            foreach (Transform child in outParent)
+            {
+                if (child.gameObject.tag == "Block")
+                {
+                    child.gameObject.GetComponent<Renderer>().material = operateMat;
+                }
+            }
+
+            foreach (Transform child in inParent)
+            {
+                child.gameObject.GetComponent<Renderer>().material = notOperateMat;
+            }
+        }
+        else if (!modeManager.GetFalseInTrueOut())
+        {
+            foreach (Transform child in inParent)
+            {
+                child.gameObject.GetComponent<Renderer>().material = operateMat;
+            }
+
+            foreach (Transform child in outParent)
+            {
+                if (child.gameObject.tag == "Block")
+                {
+                    child.gameObject.GetComponent<Renderer>().material = notOperateMat;
+                }
+            }
+        }
+    }
+}
