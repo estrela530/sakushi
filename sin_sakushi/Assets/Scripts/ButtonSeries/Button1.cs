6eb4270805756579f32e14adea1ab1e8a318d003
estrela530 <daisuke.0530.otk@gmail.com>
2020-04-19 15:52:38 +0900

    シーン変更系（仮）完成

---
+++ sin_sakushi/Assets/Scripts/ButtonSeries/Button1.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Button1 : MonoBehaviour
+{
+    [SerializeField]
+    int nextSceneNum = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    //ボタンを押したときの処理メソッド
+    public void OnClickStartButton()
+    {
+        SceneManager.LoadSceneAsync(nextSceneNum);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ sin_sakushi/Assets/Scripts/Manager/SceneChengeManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChengeManager : MonoBehaviour
+{
+    [SerializeField]
+    string currentSceneName;
+    int currentSceneNum = 0;
+    string nextSceneName;
+    int nextSceneNum;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //シーン管理（切り替え用）メソッド
+    public void SceneChenge()
+    {
+        currentSceneNum += 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
